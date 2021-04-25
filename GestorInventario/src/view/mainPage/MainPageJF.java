f8daa768363fa57f994ff9144eaa44a3c47cbb7d
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-04-25 19:33:31 +0200

    MainPage añadida

---
+++ GestorInventario/src/view/mainPage/MainPageJF.java
@@ -0,0 +1,47 @@
+package view.mainPage;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class MainPageJF extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainPageJF frame = new MainPageJF();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainPageJF() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 758, 426);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(144, 238, 144));
+		panel.setBounds(0, 0, 742, 387);
+		contentPane.add(panel);
+	}
+}
