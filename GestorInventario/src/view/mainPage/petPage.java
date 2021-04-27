ddbd104110630fceff004e930b7ba8f072cdc35a
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-04-27 13:08:53 +0200

    Añadido dispose a las pages hijas de MainPage

--- GestorInventario/src/view/mainPage/petPage.java
+++ GestorInventario/src/view/mainPage/petPage.java
@@ -41,7 +41,7 @@ public class petPage extends JFrame {
 	 * Create the frame.
 	 */
 	public petPage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 758, 426);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- GestorInventario/src/view/mainPage/purchasePage.java
+++ GestorInventario/src/view/mainPage/purchasePage.java
@@ -42,7 +42,7 @@ public class purchasePage extends JFrame {
 	 * Create the frame.
 	 */
 	public purchasePage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 758, 426);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
