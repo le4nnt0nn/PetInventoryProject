f28a6d5eadc61bc3b6cabcf2f763763e72e27fb3
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-04-28 12:06:02 +0200

    Pantalla de Acción exitosa en proceso

---
+++ GestorInventario/src/view/mainPage/successfulAction.java
@@ -0,0 +1,55 @@
+package view.mainPage;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class successfulAction extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					successfulAction frame = new successfulAction();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public successfulAction() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 758, 426);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(144, 238, 144));
+		panel.setBounds(0, 0, 742, 387);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(successfulAction.class.getResource("/images/doggieSuccAction.gif")));
+		lblNewLabel.setBounds(174, 55, 402, 300);
+		panel.add(lblNewLabel);
+	}
+}
