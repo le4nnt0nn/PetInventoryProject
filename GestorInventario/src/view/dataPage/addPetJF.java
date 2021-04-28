049ab0bb1da5892a56488230bfe667f12c38336c
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-04-28 12:34:58 +0200

    DISPOSE ON CLOSE en addPetJF

--- GestorInventario/src/view/dataPage/addPetJF.java
+++ GestorInventario/src/view/dataPage/addPetJF.java
@@ -54,7 +54,7 @@ public class addPetJF extends JFrame {
 	 * Create the frame.
 	 */
 	public addPetJF() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 758, 426);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
