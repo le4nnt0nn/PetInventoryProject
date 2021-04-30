8a63d4c5c8307930a988de55b548b99f1a6ff848
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-04-30 16:52:22 +0200

    DisposeOnClose añadido para lists

--- GestorInventario/src/view/dataPage/listPurchases.java
+++ GestorInventario/src/view/dataPage/listPurchases.java
@@ -38,7 +38,7 @@ public class listPurchases extends JFrame {
 	 * Create the frame.
 	 */
 	public listPurchases() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 758, 426);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
