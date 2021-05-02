a80a56674486f06c8cc997ee2e9bfc905a94bf1a
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-02 19:14:00 +0200

    Añadido mejora en listUsers

--- GestorInventario/src/view/dataPage/listUsers.java
+++ GestorInventario/src/view/dataPage/listUsers.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import controller.Table;
 
@@ -15,12 +16,23 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class listUsers extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTable table;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
 
 	/**
 	 * Launch the application.
@@ -72,11 +84,119 @@ public class listUsers extends JFrame {
 		panel.add(btnSearch);
 		
 		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DefaultTableModel model2 = (DefaultTableModel)table.getModel();
+				int selectedRowIndex = table.getSelectedRow();
+				
+				textField_1.setText(model2.getValueAt(selectedRowIndex, 0).toString());
+				textField_2.setText(model2.getValueAt(selectedRowIndex, 1).toString());
+				textField_3.setText(model2.getValueAt(selectedRowIndex, 2).toString());
+				textField_4.setText(model2.getValueAt(selectedRowIndex, 3).toString());
+				textField_5.setText(model2.getValueAt(selectedRowIndex, 4).toString());
+				textField_6.setText(model2.getValueAt(selectedRowIndex, 5).toString());
+				textField_7.setText(model2.getValueAt(selectedRowIndex, 6).toString());
+				textField_8.setText(model2.getValueAt(selectedRowIndex, 7).toString());
+				textField_9.setText(model2.getValueAt(selectedRowIndex, 8).toString());
+			}
+		});
 		//Modelo
 		String[] usersColumns = {"idUser","Username","Passwrod","Role","Name","Lastname","Address","Birth","Phone"};
 		new Table().showTable(table, "SELECT * FROM users;",usersColumns);
 		
-		table.setBounds(26, 109, 630, 241);
+		table.setBounds(10, 109, 509, 241);
 		panel.add(table);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(623, 106, 55, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(623, 137, 89, 20);
+		panel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(623, 165, 89, 20);
+		panel.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(623, 191, 55, 20);
+		panel.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(623, 219, 89, 20);
+		panel.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(623, 247, 89, 20);
+		panel.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(623, 274, 89, 20);
+		panel.add(textField_7);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(623, 303, 89, 20);
+		panel.add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(623, 334, 89, 20);
+		panel.add(textField_9);
+		
+		JLabel lblIdUser = new JLabel("idUser");
+		lblIdUser.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblIdUser.setBounds(564, 109, 46, 14);
+		panel.add(lblIdUser);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblUsername.setBounds(564, 140, 60, 14);
+		panel.add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblPassword.setBounds(564, 168, 55, 14);
+		panel.add(lblPassword);
+		
+		JLabel lblRole = new JLabel("Role");
+		lblRole.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblRole.setBounds(564, 194, 46, 14);
+		panel.add(lblRole);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblName.setBounds(564, 222, 46, 14);
+		panel.add(lblName);
+		
+		JLabel lblLastname = new JLabel("Lastname");
+		lblLastname.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblLastname.setBounds(564, 250, 60, 14);
+		panel.add(lblLastname);
+		
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblAddress.setBounds(564, 277, 46, 14);
+		panel.add(lblAddress);
+		
+		JLabel lblBirth = new JLabel("Birth");
+		lblBirth.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblBirth.setBounds(564, 306, 46, 14);
+		panel.add(lblBirth);
+		
+		JLabel lblPhone = new JLabel("Phone");
+		lblPhone.setFont(new Font("Open Sans", Font.BOLD, 11));
+		lblPhone.setBounds(564, 337, 46, 14);
+		panel.add(lblPhone);
+		
 	}
 }
