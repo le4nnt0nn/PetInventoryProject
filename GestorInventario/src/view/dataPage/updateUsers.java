f188860b9454d5566362f03d96ec3880f752b0ce
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-05 16:41:11 +0200

    Search implementado en updateUsers

--- GestorInventario/src/view/dataPage/updateUsers.java
+++ GestorInventario/src/view/dataPage/updateUsers.java
@@ -30,7 +30,7 @@ import javax.swing.JScrollPane;
 public class updateUsers extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField textFieldSearch;
 	private JTable table;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -80,23 +80,11 @@ public class updateUsers extends JFrame {
 		lblUpdateUsersmode.setBounds(253, 11, 255, 38);
 		panel.add(lblUpdateUsersmode);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(235, 60, 255, 20);
-		panel.add(textField);
+		textFieldSearch = new JTextField();
+		textFieldSearch.setColumns(10);
+		textFieldSearch.setBounds(235, 60, 255, 20);
+		panel.add(textFieldSearch);
 		
-		JButton btnSearch = new JButton("Search");
-		btnSearch.setBackground(Color.WHITE);
-		btnSearch.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				new Sound().button_sound();
-			}
-		});
-		btnSearch.setForeground(Color.BLACK);
-		btnSearch.setFont(new Font("Open Sans Semibold", Font.PLAIN, 14));
-		btnSearch.setBounds(508, 59, 89, 23);
-		panel.add(btnSearch);
 		
 		table = new JTable();
 		table.setBounds(10, 109, 534, 241);
@@ -128,6 +116,25 @@ public class updateUsers extends JFrame {
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setViewportView(table);
+		
+
+		JButton btnSearch = new JButton("Search");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				new Table().showTable(table, "SELECT * FROM users where idUser='"+textFieldSearch.getText()+"' OR username='"+textFieldSearch.getText()+"' OR lastname='"+textFieldSearch.getText()+"' OR role='"+textFieldSearch.getText()+"' OR name='"+textFieldSearch.getText()+"' OR lastname='"+textFieldSearch.getText()+"' OR address='"+textFieldSearch.getText()+"' OR birth='"+textFieldSearch.getText()+"' OR phone='"+textFieldSearch.getText()+"';",usersColumns);
+			}
+		});
+		btnSearch.setBackground(Color.WHITE);
+		btnSearch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				new Sound().button_sound();
+			}
+		});
+		btnSearch.setForeground(Color.BLACK);
+		btnSearch.setFont(new Font("Open Sans Semibold", Font.PLAIN, 14));
+		btnSearch.setBounds(508, 59, 89, 23);
+		panel.add(btnSearch);
 		
 			
 		JButton btnUpdate = new JButton("Update");
